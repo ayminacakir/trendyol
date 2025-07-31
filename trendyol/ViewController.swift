fe098e44d6481274a3795f8925d25e29061f76d3
Aymina Çakır <114817398+ayminacakir@users.noreply.github.com>
2025-07-31 07:47:05 +0300

    Initial Commit

---
+++ trendyol/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  trendyol
+//
+//  Created by aymina on 31.07.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ trendyolTests/trendyolTests.swift
@@ -0,0 +1,17 @@
+//
+//  trendyolTests.swift
+//  trendyolTests
+//
+//  Created by aymina on 31.07.2025.
+//
+
+import Testing
+@testable import trendyol
+
+struct trendyolTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
