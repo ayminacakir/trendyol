ec230aacba764cb97c12b9446212707f8bd8d7ec
Aymina Çakır <114817398+ayminacakir@users.noreply.github.com>
2025-08-08 09:36:53 +0300

    The position of the alreadyHaveAccountButton is set

--- RegistrationScreen/RegistrationViewScreen.swift
+++ RegistrationScreen/RegistrationViewScreen.swift
@@ -93,51 +93,68 @@ class RegistrationViewScreen: UIViewController {
 
 
     }
-    
-    private func setupLayout() {
-        view.addSubview(containerView)
+   
+private func setupLayout() {
+ view.addSubview(containerView)
+ 
+ [titleLabel,
+ nameTextField,
+ emailTextField,
+ passwordTextField,
+ registerButton,
+ registerPromptLabel,
+ alreadyHaveAccountButton].forEach {
+     containerView.addSubview($0)
+ }
 
-        let bottomStack = UIStackView(arrangedSubviews: [
-            registerPromptLabel,
-            alreadyHaveAccountButton
-        ])
-        bottomStack.axis = .horizontal
-        bottomStack.spacing = 0
-        bottomStack.alignment = .center
-        bottomStack.distribution = .equalCentering
-        bottomStack.translatesAutoresizingMaskIntoConstraints = false
+ 
+ NSLayoutConstraint.activate([
+     containerView.centerYAnchor.constraint(equalTo: view.centerYAnchor),
+     containerView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 32),
+     containerView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -32),
+     containerView.heightAnchor.constraint(greaterThanOrEqualToConstant: 300)
+ ])
+ 
+ NSLayoutConstraint.activate([
+ titleLabel.topAnchor.constraint(equalTo: containerView.topAnchor, constant: 24),
+ titleLabel.leadingAnchor.constraint(equalTo: containerView.leadingAnchor, constant: 16),
+ titleLabel.trailingAnchor.constraint(equalTo: containerView.trailingAnchor, constant: -16),
+ titleLabel.heightAnchor.constraint(equalToConstant: 44),
+     
+ nameTextField.topAnchor.constraint(equalTo: titleLabel.bottomAnchor, constant: 16),
+ nameTextField.leadingAnchor.constraint(equalTo: titleLabel.leadingAnchor),
+ nameTextField.trailingAnchor.constraint(equalTo: titleLabel.trailingAnchor),
+ nameTextField.heightAnchor.constraint(equalToConstant: 44),
+ 
+ emailTextField.topAnchor.constraint(equalTo:  nameTextField.bottomAnchor, constant: 16),
+ emailTextField.leadingAnchor.constraint(equalTo:  nameTextField.leadingAnchor),
+ emailTextField.trailingAnchor.constraint(equalTo:  nameTextField.trailingAnchor),
+ emailTextField.heightAnchor.constraint(equalToConstant: 44),
+ 
+ passwordTextField.topAnchor.constraint(equalTo:  emailTextField.bottomAnchor, constant: 16),
+ passwordTextField.leadingAnchor.constraint(equalTo:  emailTextField.leadingAnchor),
+ passwordTextField.trailingAnchor.constraint(equalTo: emailTextField.trailingAnchor),
+ passwordTextField.heightAnchor.constraint(equalToConstant: 44),
+     
+ registerButton.topAnchor.constraint(equalTo: passwordTextField.bottomAnchor, constant: 20),
+ registerButton.leadingAnchor.constraint(equalTo: passwordTextField.leadingAnchor),
+ registerButton.trailingAnchor.constraint(equalTo: passwordTextField.trailingAnchor),
+ registerButton.heightAnchor.constraint(equalToConstant: 44),
+     
+     
+ registerPromptLabel.topAnchor.constraint(equalTo: registerButton.bottomAnchor, constant: 16),
+ registerPromptLabel.centerXAnchor.constraint(equalTo: containerView.centerXAnchor, constant: -30),
+     
+ alreadyHaveAccountButton.centerYAnchor.constraint(equalTo: registerPromptLabel.centerYAnchor),
+ alreadyHaveAccountButton.leadingAnchor.constraint(equalTo: registerPromptLabel.trailingAnchor, constant: 5),
+     
+ alreadyHaveAccountButton.bottomAnchor.constraint(lessThanOrEqualTo: containerView.bottomAnchor, constant: -24),
 
-        let stackView = UIStackView(arrangedSubviews: [
-            titleLabel,
-            nameTextField,
-            emailTextField,
-            passwordTextField,
-            registerButton,
-            bottomStack
-        ])
-        stackView.axis = .vertical
-        stackView.spacing = 16
-        stackView.alignment = .fill
-        stackView.translatesAutoresizingMaskIntoConstraints = false
-
-        containerView.addSubview(stackView)
-
-        NSLayoutConstraint.activate([
-            containerView.centerYAnchor.constraint(equalTo: view.centerYAnchor),
-            containerView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 32),
-            containerView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -32),
-            containerView.heightAnchor.constraint(greaterThanOrEqualToConstant: 350),
-
-            stackView.topAnchor.constraint(equalTo: containerView.topAnchor, constant: 24),
-            stackView.leadingAnchor.constraint(equalTo: containerView.leadingAnchor, constant: 16),
-            stackView.trailingAnchor.constraint(equalTo: containerView.trailingAnchor, constant: -16),
-            stackView.bottomAnchor.constraint(lessThanOrEqualTo: containerView.bottomAnchor, constant: -24),
-
-            registerButton.heightAnchor.constraint(equalToConstant: 44)
-        ])
-    }
-
-
+ 
+ 
+ ])
+ 
+}
     
     @objc private func registerTapped() {
         guard let name = nameTextField.text, !name.isEmpty,
